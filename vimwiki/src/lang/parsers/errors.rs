c28ddea825da88a47bc2be165585be842a57d24e
Chip Senkbeil <chip@senkbeil.org>
2020-10-18 10:23:02 -0500

    Comment out eager usage of line/column due to performance cost

--- vimwiki/src/lang/parsers/errors.rs
+++ vimwiki/src/lang/parsers/errors.rs
@@ -45,8 +45,8 @@ impl LangParserError {
     }
 
     pub fn from_ctx(input: &Span, ctx: &'static str) -> Self {
-        let line = input.line();
-        let column = input.column();
+        let line = 1; //input.line();
+        let column = 1; //input.column();
         Self {
             ctx: ctx.to_string(),
             sample: input
@@ -64,8 +64,8 @@ impl LangParserError {
 
 impl ParseError<Span<'_>> for LangParserError {
     fn from_error_kind(input: Span, kind: ErrorKind) -> Self {
-        let line = input.line();
-        let column = input.column();
+        let line = 1; //input.line();
+        let column = 1; //input.column();
         Self {
             ctx: kind.description().to_string(),
             sample: input
@@ -87,8 +87,8 @@ impl ParseError<Span<'_>> for LangParserError {
     }
 
     fn from_char(input: Span, c: char) -> Self {
-        let line = input.line();
-        let column = input.column();
+        let line = 1; //input.line();
+        let column = 1; //input.column();
         Self {
             ctx: format!("Char {}", c),
             sample: input
@@ -113,8 +113,8 @@ impl ParseError<Span<'_>> for LangParserError {
     }
 
     fn add_context(input: Span, ctx: &'static str, other: Self) -> Self {
-        let line = input.line();
-        let column = input.column();
+        let line = 1; //input.line();
+        let column = 1; //input.column();
         Self {
             ctx: ctx.to_string(),
             sample: input
